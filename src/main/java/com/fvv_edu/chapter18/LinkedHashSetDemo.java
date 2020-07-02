9f13f04b5c8c39e50934b806166f0ebda2baea25
Viktor Filatov <fvv.edu@gmail.com>
2020-07-02 10:26:46 +0300

    add chapter18

---
+++ src/main/java/com/fvv_edu/chapter18/LinkedHashSetDemo.java
@@ -0,0 +1,19 @@
+package main.java.com.fvv_edu.javacore.chapter18;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+    public static void main(String[] args) {
+        LinkedHashSet<String> hs = new LinkedHashSet<String>();
+
+        hs.add("Бета");
+        hs.add("Альфа");
+        hs.add("Эта");
+        hs.add("Гамма");
+        hs.add("Эпсилон");
+        hs.add("Омега");
+
+        System.out.println(hs);
+    }
+}
+
---
+++ src/main/java/com/fvv_edu/chapter18/LinkedListDemo.java
@@ -0,0 +1,35 @@
+package main.java.com.fvv_edu.javacore.chapter18;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<String> ll= new LinkedList<String>();
+
+        ll.add("F");
+        ll.add("B");
+        ll.add("D");
+        ll.add("E");
+        ll.add("C");
+        ll.add("F");
+        ll.addLast("Z");
+        ll.addFirst("A");
+        ll.add(1,"A2");
+
+        System.out.println("Исходное содержимое связного списка ll: " + ll);
+
+        ll.remove("F");
+        ll.remove(2);
+        System.out.println("Содержимое связного списка ll после удаления элементов: " + ll);
+
+        ll.removeFirst();
+        ll.removeLast();
+
+        System.out.println("Содержимое связного списка ll после удаления первого и послежнего элементов " + ll);
+
+        String val = ll.get(2);
+        ll.set(2, val + " изменено");
+
+        System.out.println("Содержимое связного списка ll после изменения: " + ll);
+    }
+}
